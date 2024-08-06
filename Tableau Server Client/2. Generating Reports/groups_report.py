0da284c4de5c34d7a2fc8aca71eb3aa3b224db73
Chris Perry <41455335+CodingOnWindowsOS@users.noreply.github.com>
2024-08-05 20:39:26 -0500

    Update groups_report.py

--- Tableau Server Client/2. Generating Reports/groups_report.py
+++ Tableau Server Client/2. Generating Reports/groups_report.py
@@ -6,11 +6,12 @@ import tableauserverclient as tsc
 
 def main():
     # Setup
-    # Store token name and token value in variables.
+    # Store token name, token value, and site ID in variables.
     token_name = 'TSM'
     token_value = keyring.get_password('Tableau Server Management', 'TSM')
-    # Create authentication object using the token details.
-    tableau_auth = tsc.PersonalAccessTokenAuth(token_name=token_name, personal_access_token=token_value, site_id='sqlshortreads')
+    site_id = 'sqlshortreads'
+    # Create authentication object using the token and site ID details.
+    tableau_auth = tsc.PersonalAccessTokenAuth(token_name=token_name, personal_access_token=token_value, site_id=site_id)
     # Create a tableau server client object using specified server URL.
     server = tsc.Server('https://10ax.online.tableau.com')
     # Disable certificate verification. The next line of code may be required due to certificate issues.
